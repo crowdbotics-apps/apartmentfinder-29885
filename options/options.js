b2b97d1804a09e87968cdc838efcf1b5fa95233f
Crowdbotics <team@crowdbotics.com>
2021-08-17 16:42:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "apartmentfinder_29885Identifier",
+  url: "https://apartmentfinder_29885Identifier.botics.co",
+  api: "https://apartmentfinder_29885Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
